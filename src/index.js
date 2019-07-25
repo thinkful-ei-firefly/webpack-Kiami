82f633f25c12bb380ef26fea9eeb75a64106c879
Kiami Nichols <kiamimariah@yahoo.com>
2019-07-25 10:58:39 -0400

    change in position

---
+++ src/index.js
@@ -0,0 +1,18 @@
+
+import shoppingList from './shopping-list';
+import store from './store';
+import api from './api';
+
+/* global shoppingList, store, api */
+
+$(document).ready(function() {
+  shoppingList.bindEventListeners();
+
+  // On initial load, fetch Shopping Items and render
+  api.getItems()
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    })
+    .catch(err => console.log(err.message));
+});
